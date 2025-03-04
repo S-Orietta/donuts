47dcb5d3835aa3e2fa9138499a7ccf901c0c20cc
S-Orietta <150481175+S-Orietta@users.noreply.github.com>
2025-03-05 01:35:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,81 +1,41 @@
-
-
 $(function(){
-  
-	// 「.social-icon」にマウスが乗ったときに、そのfont-sizeを30pxに変更し、
-	// 離れたときにはfont-sizeを24pxに。
-  $('.items-1').hover(
-	function(){
-	  $(this).transform({
-		'scale':'1.2'
-	  },300);
-	},
-	function(){
-	  $(this).animate({
-		'scale':'1.0'
-	  },300);
-	}
-	
-	)
+    $('.peti').hover(
+        function() {
+          $('.peti-info').fadeIn(300);
+        },
+        function() {
+          $('.peti-info').fadeOut(300);
+        }
+      );
 
-	
-	  $('.items-1').click(function() {
-		$('#item-modal-1').fadeIn();
-	  });
-	  $('.close-modal').click(function() {
-		$('#item-modal-1').fadeOut();
-	  });
+      $('.chocola').hover(
+        function() {
+          $('.chocola-info').fadeIn(300);
+        },
+        function() {
+          $('.chocola-info').fadeOut(300);
+        }
+        
+      );
+});
 
+$(document).ready(function() {
+  var images = [
+      'url("images/P1030021.JPG")',
+      'url("images/P1030053.jpg")',
+      'url("images/P1030006.JPG")'
+      // ここに他の画像のURLを追加できます
+  ];
 
-	  $('.items-2').click(function() {
-		$('#item-modal-2').fadeIn();
-	  });
-	  $('.close-modal').click(function() {
-		$('#item-modal-2').fadeOut();
-	  });
-	
-	  
-	  $('.items-3').click(function() {
-		$('#item-modal-3').fadeIn();
-	  });
-	  $('.close-modal').click(function() {
-		$('#item-modal-3').fadeOut();
-	  });
+  var currentIndex = 0;
+  var $bgImages = $('.bg-image');
+  $bgImages.eq(0).css('background-image', images[0]).show();
 
-	  $('.items-4').click(function() {
-		$('#item-modal-4').fadeIn();
-	  });
-	  $('.close-modal').click(function() {
-		$('#item-modal-4').fadeOut();
-	  });
-	
-	  $('.items-5').click(function() {
-		$('#item-modal-5').fadeIn();
-	  });
-	  $('.close-modal').click(function() {
-		$('#item-modal-5').fadeOut();
-	  });
-	 
-	  $('.items-6').click(function() {
-		$('#item-modal-6').fadeIn();
-	  });
-	  $('.close-modal').click(function() {
-		$('#item-modal-6').fadeOut();
-	  });
-	
-	  $('.items-7').click(function() {
-		$('#item-modal-7').fadeIn();
-	  });
-	  $('.close-modal').click(function() {
-		$('#item-modal-7').fadeOut();
-	  });
-
-	  $('.items-8').click(function() {
-		$('#item-modal-8').fadeIn();
-	  });
-	  $('.close-modal').click(function() {
-		$('#item-modal-8').fadeOut();
-	  });
-
-	
-  });
+  setInterval(function() {
+      var nextIndex = (currentIndex + 1) % images.length;
+      $bgImages.eq(nextIndex).css('background-image', images[nextIndex]).fadeIn(1000, function() {
+          $bgImages.eq(currentIndex).fadeOut(1000);
+          currentIndex = nextIndex;
+      });
+  }, 4000); // 画像を切り替える間隔（ミリ秒）
+});
